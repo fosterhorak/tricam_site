18537ccf1220740d557eea35bd18d8528a5e46b0
fosterhorak <foster.f.horak@gmail.com>
2021-06-08 05:33:28 -0500

    nav bar and video section formatting

--- app.js
+++ app.js
@@ -18,99 +18,64 @@ menu.addEventListener('click', mobileMenu);
 const highlightMenu = () => {
     const elem = document.querySelector('.highlight');
 
-    const selection1 = document.querySelector('#section1-link');
-    const selection2 = document.querySelector('#section2-link');
-    const selection3 = document.querySelector('#section3-link');
-    const selection4 = document.querySelector('#section4-link');
-    const selection5 = document.querySelector('#section5-link');
-    const selection6 = document.querySelector('#section6-link');
+    const selection_welcome = document.querySelector('#welcome-link');
+    const selection_prod_units = document.querySelector('#prod_units-link');
+    const selection_tech = document.querySelector('#tech-link');
+    const selection_credits = document.querySelector('#credits-link');
 
     let scrollPos = window.scrollY;
-    // console.log(scrollPos);
-
-    let section1_2Break = 550;
-    let section2_3Break = 1400;
-    let section3_4Break = 2400;
-    let section4_5Break = 3200;
-    let section5_6Break = 3900;
+    console.log(scrollPos);
 
 
-    // adds 'highlight' class to my menu items 
-    if (window.innerWidth > 960 && scrollPos <= 1) {
-        selection1.classList.remove('highlight');
-        selection2.classList.remove('highlight');
-        selection3.classList.remove('highlight');
-        selection4.classList.remove('highlight');
-        selection5.classList.remove('highlight');
-        selection6.classList.remove('highlight');
-        return;
-        
-    }   else if (window.innerWidth > 960 && scrollPos < section1_2Break) {
-        selection1.classList.add('highlight');
-        selection2.classList.remove('highlight');
-        selection3.classList.remove('highlight');
-        selection4.classList.remove('highlight');
-        selection5.classList.remove('highlight');
-        selection6.classList.remove('highlight');
-        return;
-        
-    }    else if (window.innerWidth > 960 && scrollPos < section2_3Break) {
-        selection2.classList.add('highlight');
-        selection1.classList.remove('highlight');
-        selection3.classList.remove('highlight');
-        selection4.classList.remove('highlight');
-        selection5.classList.remove('highlight');
-        selection6.classList.remove('highlight');
-        return;
-        
-    }   else if (window.innerWidth > 960 && scrollPos < section3_4Break) {
-        selection3.classList.add('highlight');
-        selection1.classList.remove('highlight');
-        selection2.classList.remove('highlight');
-        selection4.classList.remove('highlight');
-        selection5.classList.remove('highlight');
-        selection6.classList.remove('highlight');
-        return;
-        
-    }  else if (window.innerWidth > 960 && scrollPos < section4_5Break) {
-        selection4.classList.add('highlight');
-        selection1.classList.remove('highlight');
-        selection2.classList.remove('highlight');
-        selection3.classList.remove('highlight');
-        selection5.classList.remove('highlight');
-        selection6.classList.remove('highlight');
 
-        return;
+    // adds 'highlight' class to my menu items at innerWidth > 960 [do this again for more narrow widths...]
+
+    // at around 960 screen width...
+    let welcome_prod_unitsBreak1 = 550;
+    let prod_units_techBreak1 = 2400;
+    let tech_creditsBreak1 = 3820;
+
     
-    } else if (window.innerWidth > 960 && scrollPos < section5_6Break) {
-        selection5.classList.add('highlight');
-        selection1.classList.remove('highlight');
-        selection2.classList.remove('highlight');
-        selection3.classList.remove('highlight');
-        selection4.classList.remove('highlight');
-        selection6.classList.remove('highlight');
-
+    if (window.innerWidth > 960 && scrollPos <= 1) {
+        selection_welcome.classList.remove('highlight');
+        selection_prod_units.classList.remove('highlight');
+        selection_tech.classList.remove('highlight');
+        selection_credits.classList.remove('highlight');
         return;
-    }   else if (window.innerWidth > 960 && scrollPos >= section5_6Break) {
-        selection6.classList.add('highlight');
-        selection1.classList.remove('highlight');
-        selection2.classList.remove('highlight');
-        selection3.classList.remove('highlight');
-        selection4.classList.remove('highlight');
-        selection5.classList.remove('highlight');
+        
+    }   else if (window.innerWidth > 960 && scrollPos < welcome_prod_unitsBreak1) {
+        selection_welcome.classList.add('highlight');
+        selection_prod_units.classList.remove('highlight');
+        selection_tech.classList.remove('highlight');
+        selection_credits.classList.remove('highlight');
+        return;
+        
+    }    else if (window.innerWidth > 960 && scrollPos < prod_units_techBreak1) {
+        selection_welcome.classList.remove('highlight');
+        selection_prod_units.classList.add('highlight');
+        selection_tech.classList.remove('highlight');
+        selection_credits.classList.remove('highlight');
+        return;
+        
+    }   else if (window.innerWidth > 960 && scrollPos < tech_creditsBreak1) {
+        selection_welcome.classList.remove('highlight');
+        selection_prod_units.classList.remove('highlight');
+        selection_tech.classList.add('highlight');
+        selection_credits.classList.remove('highlight');
+        return;
+    }   else if (window.innerWidth > 960 && scrollPos >= tech_creditsBreak1) {
+        selection_welcome.classList.remove('highlight');
+        selection_prod_units.classList.remove('highlight');
+        selection_tech.classList.remove('highlight');
+        selection_credits.classList.add('highlight');
         return;
     
     }
-
-
     if ( (elem && window.innerWidth > 960 && scrollPos < 600) || elem ){
         elem.classList.remove('highlight');
         return;
         
     }
-    
-        
-
 };
 
 window.addEventListener('scroll', highlightMenu);
